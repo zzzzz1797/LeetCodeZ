81f1ec6807e1a1e3b3af8be5ba7d0a6002d3cd9f
zzzzz1797 <979652917@qq.com>
2020-03-26 23:27:26 +0800

    修改不同路径2的解法

--- algorithm/LeetCode_63_不同路径II.py
+++ algorithm/LeetCode_63_不同路径II.py
@@ -27,52 +27,92 @@ from typing import List
 
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        return self.dp_2(obstacleGrid)
+        if obstacleGrid[0][0] == 1:
+            # 开始就有障碍物，直接返回0
+            return 0
+        return self.dp_1(obstacleGrid)
 
     @classmethod
     def dp_1(cls, obstacleGrid: List[List[int]]) -> int:
         """
-            时间复杂度：O(m*n)
-            空间复杂度：O(1)
+            定义dp数组  dp[i][j] 代表下标为i和j的的最多路径数
+            定义状态转移方程：
+                dp[i][j] = dp[i+1][j] + dp[i][j-1]
+
+            时间复杂度：
+                O(m*n)
+            空间复杂度：
+                O(m*n)
         """
-        res = 0
-        if obstacleGrid and obstacleGrid[0]:
-            if obstacleGrid[0][0] == 1:
-                # 表示一开始就有障碍物
-                return res
-            obstacleGrid[0][0] = 1
-            # 初始化边界
-            for i in range(1, len(obstacleGrid)):
-                obstacleGrid[i][0] = 0 if obstacleGrid[i][0] == 1 else obstacleGrid[i - 1][0]
+        row_len = len(obstacleGrid)
+        col_len = row_len and len(obstacleGrid[0])
 
-            for j in range(1, len(obstacleGrid[0])):
-                obstacleGrid[0][j] = 0 if obstacleGrid[0][j] == 1 else obstacleGrid[0][j - 1]
+        dp = [[0] * col_len for i in range(row_len)]
 
-            for i in range(1, len(obstacleGrid)):
-                for j in range(1, len(obstacleGrid[0])):
-                    if obstacleGrid[i][j] == 0:
-                        obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1]
-                    else:
-                        obstacleGrid[i][j] = 0
-            res = obstacleGrid[-1][-1]
-        return res
+        # 初始化dp 因为第一行和第一列的值是可以确定的
+        dp[0][0] = 1
+        for i in range(1, row_len):
+            dp[i][0] = 0 if obstacleGrid[i][0] == 1 else dp[i - 1][0]
+
+        for j in range(1, col_len):
+            dp[0][j] = 0 if obstacleGrid[0][j] == 1 else dp[0][j - 1]
+
+        for i in range(1, row_len):
+            for j in range(1, col_len):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] if obstacleGrid[i][j] != 1 else 0
+        return dp[-1][-1]
 
     @classmethod
     def dp_2(cls, obstacleGrid: List[List[int]]) -> int:
         """
-            使用一个一维数组来记录当前位置的路径数
-            将二维数组压缩到一维数组,逐行计算当前最新路径条数，并覆盖上一行对应的路径条数。
-            时间复杂度：O(m*n)
-            空间负载度：O(n)
+            优化空间复杂度
+                dp_1 中开辟了一个m*n数组，但是在循环遍历的时候，其实只用到当前行的上一行，所以可以加空间复杂度压缩到O(n)
+
+            时间复杂度：
+                O(m*n)
+            空间复杂度：
+                O(n)
+
         """
-        res = 0
-        if obstacleGrid and obstacleGrid[0]:
-            row_len = len(obstacleGrid)
-            col_len = len(obstacleGrid[0])
-            # 初始化这个一维数组
-            table = [1] + [0] * (col_len - 1)
-            for i in range(row_len):
-                for j in range(col_len):
-                    table[j] = 0 if obstacleGrid[i][j] == 1 else table[j - 1] + table[j]
-            res = table[-2]
-        return res
+        row_len = len(obstacleGrid)
+        col_len = row_len and len(obstacleGrid)
+
+        dp = [0] * col_len
+        # 初始化一下数组
+        dp[0] = 1
+        for i in range(1, col_len):
+            dp[i] = 0 if obstacleGrid[0][i] == 1 else dp[i - 1]
+
+        for i in range(1, row_len):
+            for j in range(col_len):
+                target = obstacleGrid[i][j]
+                if j == 0:
+                    dp[j] = 0 if target == 1 else 1
+                else:
+                    dp[j] = 0 if target == 1 else dp[j - 1] + dp[j]
+        return dp[row_len - 1]
+
+    @classmethod
+    def dp_3(cls, obstacleGrid: List[List[int]]) -> int:
+        """
+        相对于dp_2来说，继续压缩空间复杂度，复用给定的数组，则空间复杂度可以下降到O(1)。但是破坏了给定数组的内容。
+        时间复杂度：
+            O(m*n)
+        空间复杂度：
+            O(1)
+        """
+        row_len = len(obstacleGrid)
+        col_len = row_len and len(obstacleGrid[0])
+
+        obstacleGrid[0][0] = 1
+
+        for i in range(1, row_len):
+            obstacleGrid[i][0] = 0 if obstacleGrid[i][0] == 1 else obstacleGrid[i - 1][0]
+
+        for j in range(1, col_len):
+            obstacleGrid[0][j] = 0 if obstacleGrid[0][j] == 1 else obstacleGrid[0][j - 1]
+
+        for i in range(1, row_len):
+            for j in range(1, col_len):
+                obstacleGrid[i][j] = 0 if obstacleGrid[i][j] == 1 else obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1]
+        return obstacleGrid[-1][-1]
