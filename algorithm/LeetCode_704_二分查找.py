e2a9cce7746b38ababa3522100630bc3a17e361c
zzzzz1797 <979652917@qq.com>
2020-05-08 18:00:56 +0800

    修改二分查找

--- algorithm/LeetCode_704_二分查找.py
+++ algorithm/LeetCode_704_二分查找.py
@@ -29,3 +29,43 @@ class Solution:
             else:
                 start = mid + 1
         return -1
+
+    @classmethod
+    def solve_1(cls, nums: List[int], target: int):
+        """
+        找到第一个出现的target
+        :param nums:
+        :param target:
+        :return:
+        """
+        start = 0
+        end = len(nums) - 1
+        while start < end:
+            mid = (start + end) // 2
+            if nums[mid] < target:
+                start = mid + 1
+            else:
+                end = mid
+
+        return start if nums[start] == target else -1
+
+    @classmethod
+    def solve_2(cls, nums: List[int], target: int):
+        """
+            找到最后出现的target
+        """
+        start = 0
+        end = len(nums)
+
+        while start < end:
+            mid = (start + end) // 2
+            if nums[mid] <= target:
+                start = mid + 1
+            else:
+                end = mid
+        return start - 1 if nums[start - 1] == target else -1
+
+
+if __name__ == '__main__':
+    print(Solution.solve_1([-1, 0, 3, 5, 9, 9, 9, 9, 9, 12], 9))
+    print(Solution.solve_2([-1, 0, 3, 5, 9, 9, 9, 9, 9, 12], 9))
