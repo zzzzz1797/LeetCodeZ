ac83064703c1d2dfcb125a1fa5db29ea1e7b4aa9
linhao.zhao <linhao.zhao@qingteng.cn>
2020-12-03 09:48:06 +0800

    修改每日一题

--- algorithm/LeetCode_204_计算质数.py
+++ algorithm/LeetCode_204_计算质数.py
@@ -17,12 +17,15 @@ class Solution:
             厄拉多塞筛法：要得到自然数n以内的全部质数，必须把不大于根号n的所有质数的倍数剔除，剩下的就是质数。
             算术基本定理：任何一个合数(非质数)，都可以以唯一的形式被写成有限个质数的乘积，即分解质因数。
         """
-        pass
-        if n < 2:
-            return 0
-        is_prime = [1] * n
+        is_primes_list = [1] * n
 
-        for i in range(2, int(n ** 0.5) + 1):
-            if is_prime[i]:
-                is_prime[i * i:n:i] = [0] * ((n - 1 - i * i) // i + 1)
-        return sum(is_prime)
+        res = 0
+        for i in range(2, n):
+            if is_primes_list[i] == 1:
+                res += 1
+            tmp_i = i
+            while tmp_i * i < n:
+                is_primes_list[tmp_i * i] = 0
+                tmp_i += 1
+
+        return res
